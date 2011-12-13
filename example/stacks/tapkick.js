50f8eb7a2141005d13b94e3f7f67620e994556c7
Russell Haering <russell.haering@rackspace.com>
2011-12-12 16:54:13 -0800

    example: use taskSpawn

--- example/stacks/tapkick.js
+++ example/stacks/tapkick.js
@@ -8,37 +8,6 @@ var misc = require('util/misc');
 var sprintf = require('util/sprintf');
 var git = require('util/git');
 
-function execute(baton, args, cmd, opts, msg, callback) {
-  cmdStr = cmd.join(' ');
-  baton.log.info(cmd)
-  if (args.dryrun) {
-    baton.log.infof('dry run, skipping command: ${cmd}', {
-      cmd: cmdStr
-    });
-    callback();
-    return;
-  }
-
-  baton.log.infof('executing command: ${cmd}', {
-    cmd: cmdStr
-  });
-
-  misc.spawn(cmd, opts, function(err, stdout) {
-    if (err) {
-      baton.log.error('error executing command', {
-        cmd: cmdStr,
-        err: err,
-        stdout: stdout
-      })
-    } else {
-      baton.log.info(msg, {
-        cmd: cmdStr,
-        stdout: stdout
-      })
-    }
-    callback(err);
-  });
-};
 
 exports.get_deployedRevision = function(args, callback) {
   git.revParse(this.config.tapkick_dir, 'HEAD', function(err, stdout) {
@@ -46,10 +15,17 @@ exports.get_deployedRevision = function(args, callback) {
   });
 };
 
+
 exports.task_deploy = function(stack, baton, args, callback) {
-  cmd = ['git', 'pull', 'origin', 'master']
-  opts = { cwd: stack.config.tapkick_dir, env: process.env }
-  execute(baton, args, cmd, opts, 'run git pull', callback);
+  var cmd = ['git', 'pull', 'origin', 'master'],
+      opts = {cwd: stack.config.tapkick_dir, env: process.env};
+
+  misc.taskSpawn(baton, args, cmd, opts, function(err, stdout) {
+    if (!err) {
+      baton.log.info('ran git pull');
+    }
+    callback(err);
+  });
 };
 
 exports.targets = {
