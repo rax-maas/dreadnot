619a0ee80033e020b76aa2c7772a4c55578f8f04
Tomaz Muraus <tomaz@tomaz.me>
2011-12-17 16:47:56 -0800

    Use file session store.

---
+++ node_modules/connect-fs/test.js
@@ -0,0 +1,62 @@
+
+/**
+ * Module dependencies.
+ */
+
+var assert = require('assert')
+  , connect = require('connect')
+  , FSStore = require('./lib/connect-fs.js')(connect);
+
+var store = new FSStore;
+var store_alt = new FSStore({ dir: 'sessions2' });
+
+store.client.on('connect', function(){
+  // #set()
+  store.set('123', { cookie: { maxAge: 2000 }, name: 'tnantoka' }, function(err, ok){
+    assert.ok(!err, '#set() got an error');
+    assert.ok(ok, '#set() is not ok');
+    
+    // #get()
+    store.get('123', function(err, data){
+      assert.ok(!err, '#get() got an error');
+      assert.deepEqual({ cookie: { maxAge: 2000 }, name: 'tnantoka' }, data);
+  
+      // #length()
+      store.length(function(err, len){
+        assert.ok(!err, '#length() got an error');
+        assert.equal(1, len, '#length() with keys');
+
+        // #db option
+        store_alt.length(function (err, len) {
+          assert.ok(!err, '#alt db got an error');
+          assert.equal(0, len, '#alt db with keys'); 
+
+          // #clear()
+          store.clear(function(err, ok){
+            assert.ok(!err, '#clear()');
+            assert.ok(ok, '#clear()');
+
+            // #length()
+            store.length(function(err, len){
+              assert.ok(!err, '#length()');
+              assert.equal(0, len, '#length() without keys');
+
+              // #set null
+              store.set('123', { cookie: { maxAge: 2000 }, name: 'tnantoka' }, function(){
+                store.destroy('123', function(){
+                  store.length(function(err, len){
+                   assert.equal(0, len, '#set() null');
+                   console.log('done');
+//                   store.client.end(); 
+//                   store_alt.client.end();
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    })
+  });
+});
+
